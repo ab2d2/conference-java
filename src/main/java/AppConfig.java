393d6bca6f1f40339773db353bb566d8c0ea615b
ab2d2 <abdul.rahimi@capgemini.com>
2020-08-05 13:29:45 +0100

    remove config and use stereotypes

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -4,22 +4,27 @@ import com.pluralsight.service.SpeakerServiceImpl;
 import com.pluralsight.service.SpeakerService;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
+@ComponentScan({"com.pluralsight"}) //start scanning here for beans to autowire
 public class AppConfig {
-
+    /*
     @Bean(name = "speakerService")
     @Scope(value = BeanDefinition.SCOPE_SINGLETON)
     public SpeakerService getSpeakerService() {
         SpeakerServiceImpl service = new SpeakerServiceImpl();
         return service;
     }
+    */
 
+    /*
     @Bean(name = "speakerRepository")
     public SpeakerRepository getSpeakerRepository() {
         return new HibernateSpeakerRepositoryImpl();
     }
+     */
 
 }
--- src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -3,15 +3,18 @@ package com.pluralsight.service;
 import com.pluralsight.model.Speaker;
 import com.pluralsight.repository.SpeakerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service("speakerService")
 public class SpeakerServiceImpl implements SpeakerService {
     public SpeakerServiceImpl () {
         System.out.println("SpeakerserviceImpl no args constructor");
     }
     private SpeakerRepository repository;
 
+    @Autowired //constructor injection
     public SpeakerServiceImpl (SpeakerRepository speakerRepository) {
         System.out.println("SpeakerserviceImpl repository constructor");
         repository = speakerRepository;
@@ -21,7 +24,7 @@ public class SpeakerServiceImpl implements SpeakerService {
         return repository.findAll();
     }
 
-    @Autowired
+    //@Autowired //setter injection
     public void setRepository(SpeakerRepository repository) {
         System.out.println("SpeakerserviceImpl setter");
 
