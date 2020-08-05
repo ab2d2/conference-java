e9e4777b10e5f829eea230bb831c58fa552b1da2
ab2d2 <abdul.rahimi@capgemini.com>
2020-08-05 11:55:59 +0100

    used constructor injection

--- src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -1,13 +1,16 @@
 package com.pluralsight.service;
 
 import com.pluralsight.model.Speaker;
-import com.pluralsight.repository.HibernateSpeakerRepositoryImpl;
 import com.pluralsight.repository.SpeakerRepository;
 
 import java.util.List;
 
 public class SpeakerServiceImpl implements SpeakerService {
     private SpeakerRepository repository;
+
+    public SpeakerServiceImpl (SpeakerRepository speakerRepository) {
+        repository = speakerRepository;
+    }
 
     public List<Speaker> findAll() {
         return repository.findAll();
